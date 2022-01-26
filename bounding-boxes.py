441b9fa42e1554dfd67fc0989156310bd1fa7149
Thura Aung <thuraaung.it.2017@kyauksetu.edu.mm>
2022-01-27 00:00:45 +0630

    Update bounding-boxes.py

--- bounding-boxes.py
+++ bounding-boxes.py
@@ -2,8 +2,21 @@ import cv2
 import numpy as np
 import imutils
 
-inFile = "processed.jpg"
-outFile = "bounding-boxes.jpg"
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description='Syllable-based word segmentation for Myanmar language')
+parser.add_argument('-i', '--input', type=str, help='input file', required=True)
+parser.add_argument('-o', '--output', type=str, default='processed.jpg', help='output file', required=True)
+parser.add_argument('-c', '--crop', type=str, default='on', help='one digit per one image use on or else off', required=True)
+parser.add_argument('-p', '--path', type=str, default='./', help='path if you want to crop', required=False)
+
+args = parser.parse_args()
+
+inputFile = getattr(args, 'input')
+outFile = getattr(args, 'output')
+crop = getattr(args, 'crop') 
+path = getattr(args, 'path') 
 
 def bounding(img):
     image = cv2.imread(img)
@@ -14,10 +27,7 @@ def bounding(img):
     ret,out_binary=cv2.threshold(gray ,127,255,cv2.THRESH_BINARY_INV)
 
     img_dilation = cv2.dilate(out_binary, None, iterations=2)
-
-    #find contours
-    # ctrs, hier = cv2.findContours(img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
+    
     contours, h = cv2.findContours(img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
     for i, ctr in enumerate(contours):
@@ -29,6 +39,9 @@ def bounding(img):
         cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
         
+        if crop == 'on':
+            im = image[y:y+h, x:x+w]
+            cv2.imwrite(path, im)
 
         cv2.rectangle(image,(x,y),( x + w, y + h ),(90,0,255),2)
     return image
